d48b97f41025888d3879a25ee194e7a96bbf1436
labankibet <laban.kibet@moringaschool.com>
2023-08-19 12:07:27 +0300

    Added Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Header from "./Header";
 import blogData from "../data/blog";
+
 
 console.log(blogData);
 
@@ -8,7 +10,9 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
     </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import blogData from "../data/blog";
+
+console.log(blogData);
+
+function Header({name}) {
+  return (
+    <header>
+      <h1>{name}</h1>
+    </header>
+  );
+}
+
+export default Header;
